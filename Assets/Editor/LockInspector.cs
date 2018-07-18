f5f618f1b85affcb73246a8363cc83e387d84601
Harshit Doodwal <doodwal21@gmail.com>
2018-07-19 00:57:49 +0530

    Commit #2
    Added Lock Inspector Script

---
+++ Assets/Editor/LockInspector.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public static class LockInspector
+{
+	[MenuItem ("GameObject/Lock Inspector", false, -1)]
+	public static void Lock ()
+	{
+		ActiveEditorTracker.sharedTracker.isLocked = !ActiveEditorTracker.sharedTracker.isLocked;
+		ActiveEditorTracker.sharedTracker.ForceRebuild ();
+	}
+}
